9f4441d0573790a7a44dddbefd31d093b3222be4
Kirsten0604 <hungrytacocat@gmail.com>
2017-07-29 15:39:48 -1000

    CPU

--- app/analytics.py
+++ app/analytics.py
@@ -1,16 +1,23 @@
 import time
 from time import clock
-
+import os
 
 #python
 start = time.clock()
 print("Language detected: Python")
 print("Runtime: %s" % (time.clock() - start));
+print("Memory Usage (CPU): %s" % (time.CLOCK_PROCESS_CPUTIME_ID))
+print("CPU TIME CLOCK: %s" % (time.CLOCK_THREAD_CPUTIME_ID))
+# print("Memory Usage: %s" % (os.stat)
 
 #javascript
 print("Language detected: JavaScript")
 print("Runtime: %s" % (time.clock() - start));
+print("Memory Usage (CPU): %s" % (time.CLOCK_PROCESS_CPUTIME_ID))
+print("CPU TIME CLOCK: %s" % (time.CLOCK_THREAD_CPUTIME_ID))
 
 #C
 print("Language detected: C")
 print("Runtime: %s" % (time.clock() - start));
+print("Memory Usage (CPU): %s" % (time.CLOCK_PROCESS_CPUTIME_ID))
+print("CPU TIME CLOCK: %s" % (time.CLOCK_THREAD_CPUTIME_ID))
