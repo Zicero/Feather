896eaa5696854ceddbffe13f694b645cb94cdc71
seanytak <seanytak@hawai.edu>
2017-07-30 02:23:51 -1000

    fixed indentation on analytics

--- app/analytics.py
+++ app/analytics.py
@@ -9,23 +9,23 @@ from subprocess import call
 from memprof import *
 
 def analyze(language):
-	f = open(analytics_file, "w")
-	start = time.clock()
-	runTime = time.time()
+    f = open(analytics_file, "w")
+    start = time.clock()
+    runTime = time.time()
     f.write("Language detected: " + language)
-    f.write("User: %s" % (getpass.getuser()))
-    f.wirte("Boot Time: %s" % (time.asctime(time.localtime(time.time()))))
-    f.write("Memory (CPU): %s" % ((time.clock() - start)))
-    f.write("Runtime: %s" % ((time.time() - runTime) * 1000))
+    f.write("User: " + (getpass.getuser()))
+    f.wirte("Boot Time: " + (time.asctime(time.localtime(time.time()))))
+    f.write("Memory (CPU): " + ((time.clock() - start)))
+    f.write("Runtime: " + ((time.time() - runTime) * 1000))
 
 
-@memprof(threshold = 1)
+@ memprof(threshold = 1)
 def memeproof():
-	RAM = ["python3", "-m", "memprof", "text.py"]
-	subprocess.call(RAM)
+    RAM = ["python3", "-m", "memprof", "text.py"]
+subprocess.call(RAM)
 
 
-analytics_file = "analytics_file.txt"	
+analytics_file = "analytics_file.txt"
 
 analyze("Python")
 memeproof()
