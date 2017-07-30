b79e7f4bc4c8b30bd2642ffa32c1ef413daae418
Kirsten0604 <hungrytacocat@gmail.com>
2017-07-29 17:08:19 -1000

    Runtime fixed

--- app/analytics.py
+++ app/analytics.py
@@ -4,20 +4,20 @@ import os
 import timeit
 
 start = time.clock()
-end = time.clock()
+runTime = time.time()
 
 #python
 print("Language detected: Python")
-print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Memory (CPU): %s" % ((time.clock() - start)))
+print("Runtime: %s" % (time.time() - runTime))
 # print("Memory Usage: %s" % (os.stat)
 
 #javascript
 print("Language detected: JavaScript")
 print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Runtime: %s" % (time.time() - runTime))
 
 #C
 print("Language detected: C")
 print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Runtime: %s" % (time.time() - runTime))
