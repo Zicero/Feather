f46acb2a07c6c09b506e1ad508114465aad61c1d
Trevin Tejero <trevin808@gmail.com>
2017-07-29 17:01:23 -1000

    Updated parsing for receive

--- app/analytics.py
+++ app/analytics.py
@@ -7,17 +7,17 @@ start = time.clock()
 end = time.clock()
 
 #python
-print("Language detected: Python")
+print ("Language detected: Python")
 print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Runtime: %s" % (end - (time.clock() - start)))
 # print("Memory Usage: %s" % (os.stat)
 
 #javascript
 print("Language detected: JavaScript")
 print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Runtime: %s" % (end - (time.clock() - start)))
 
 #C
 print("Language detected: C")
 print("Memory (CPU): %s" % (time.clock() - start))
-print("Runtime: %s" % (end - (time.clock() - start))
+print("Runtime: %s" % (end - (time.clock() - start)))
